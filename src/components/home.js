98cdb0e27e1cc6e6bdff8cf7f9318fdccd223286
AIDAN PARTINGTON <apartington@ourveryown.com.au>
2019-05-17 17:39:50 +1000

    more progress

--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,7 @@ class Home extends Component {
     super(props);
     this.state = { 
       editing: false, 
-      comments: []
+      comments: [ "Lettuce" ]
     };
   }
 
@@ -30,19 +30,19 @@ class Home extends Component {
     this.setState({comments: arr});
   }
 
-  // Javascrtipt here
   render() {
     return (
-				<div>
-          <button onClick={this.add.bind(null,'New Comment')} className="button-info create">Add New</button>
-          <div>
-            {/* {this.state.comments.map(this.eachComment)} */}
-            dfjhs
-          </div>
+				<div className="p-5">
+          <h1>Grocery List</h1>
+          <button onClick={this.add.bind(null,'New Comment')} className="button-info create mb-3 btn-success">Add New</button>
+          <Board/>
         </div>
     );
   }
 }
+
+
+
 
 class Comment extends Component {
 
@@ -63,80 +63,101 @@ class Comment extends Component {
     this.props.deleteFromBoard(this.props.index)
   }
 
-			// renderNormal: function () {
-			// 	return (
-			// 		<div className="commentContainer">
-			// 			<div className="commentText">{this.props.children}</div>
-			// 			<button onClick={this.edit} className="button-primary">Edit</button>
-			// 			<button onClick={this.remove} className="button-danger">Remove</button>
-			// 		</div>
-			// 	);
-			// },
-
-			// renderForm: function () {
-			// 	return (
-			// 		<div className="commentContainer">
-			// 			<textarea ref="newText" defaultValue={this.props.children}></textarea>
-			// 			<button onClick={this.save} className="button-success">Save</button>
-			// 		</div>
-			// 	);
-			// },
-      render() {
-          {
-            if (this.state.editing) {
-              // return this.renderForm();
-              return (<div>RenderForm</div>);
-            } else {
-              // return this.renderNormal();
-              return (<div>RenderNormal</div>);
-            }
-          }
+  render() {
+      {
+        if (this.state.editing) {
+          // return this.renderForm();
+          return (<RenderForm/>);
+        } else {
+          // return this.renderNormal();
+          return (<RenderNormal/>);
+        }
+      }
   }
 }
 
-		// var Board = React.createClass({
 
-		// 	getInitialState: function () {
-		// 		return {
-		// 			comments: []
-		// 		}
-		// 	},
 
-		// 	add: function(text) {
-		// 		console.log('add dsafjhgfd' + text);
-		// 		var arr = this.state.comments;
-		// 		arr.push(text);
-		// 		this.setState({comments: arr});
-		// 	},
+class RenderForm extends Component {
+    render() {
+      return (
+        <div>
+          <textarea ref="newText" defaultValue={this.props.children}></textarea>
+          <button onClick={this.save} className="button-success">Save</button>
+        </div>
+    );
+  }
+}
 
-		// 	removeComment: function (i) {
-		// 		console.log('removing comments ' + i);
-		// 		var arr = this.state.comments;
-		// 		arr.splice(i, 1);
-		// 		this.setState({comments: arr})
-		// 	},
+class RenderNormal extends Component {
+  render() {
+    return (
+					<div className="commentContainer">
+						<div className="commentText">{this.props.children}</div>
+						<button onClick={this.edit} className="btn-primary mr-2">Edit</button>
+						<button onClick={this.remove} className="btn-danger">Remove</button>
+					</div>
+    );
+  }
+}
 
-		// 	updateComment: function (newText, i) {
-		// 		console.log('updating comment');
-		// 		var arr = this.state.comments;
-		// 		arr[i] = newText;
-		// 		this.setState({comments: arr});
-		// 	},
+class Board extends Component {
 
-		// 	eachComment: function (text, i) {
-		// 		return (
-		// 			<Comment key={i} index={i} updateCommentText={this.updateComment} deleteFromBoard={this.removeComment}>
-		// 				{text}
-		// 			</Comment>
-		// 			);	
-		// 	},
+  constructor(props) {
+    super(props);
+    this.state = { 
+      editing: false, 
+      comments: [ "Apple" ]
+    };
+  }
 
-		// 	render: function () {
-		// 		return (
+    add(text) {
+      console.log('add dsafjhgfd' + text);
+      var arr = this.state.comments;
+      arr.push(text);
+      this.setState({comments: arr});
+    }
 
-		// 		);
-		// 	}
-		// });
+    removeComment(i) {
+      console.log('removing comments ' + i);
+      var arr = this.state.comments;
+      arr.splice(i, 1);
+      this.setState({comments: arr})
+    }
+
+		updateComment(newText, i) {
+      console.log('updating comment');
+      var arr = this.state.comments;
+      arr[i] = newText;
+      this.setState({comments: arr});
+		}
+
+    eachComment(text, i) {
+      return (
+        <Comment key={i} index={i} updateCommentText={this.updateComment} deleteFromBoard={this.removeComment}>
+          {text}
+        </Comment>
+        );	
+    }
+    
+  render() {
+      return (
+        <div className="border px-5 py-2">
+          <h3>The Board</h3>
+          { this.state.comments.map((eachComment, index) => {
+            return (
+              <div className="d-flex">
+                <div className="border d-block p-1">
+                  <h4>{eachComment}</h4>
+                  <Comment  key={index} updateCommentText={this.updateComment} deleteFromBoard={this.removeComment}/>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+      );
+    }
+  }
 
 
   export default Home;
