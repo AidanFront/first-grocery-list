6920911ed45123c8ec0b7e3a0feb7ce8a9e793e4
AIDAN PARTINGTON <apartington@ourveryown.com.au>
2019-05-22 15:14:32 +1000

    more changes

--- src/components/home.js
+++ src/components/home.js
@@ -1,163 +1,154 @@
 import React, { Component } from 'react';
 
+const textAreaNameStyle = { 
+  height : '30px',
+  width: '120px'
+}
+const textAreaPriceStyle = { 
+  height : '30px', 
+  width: '50px'
+}
+// const textAreaCategoryStyle = { height : '30px' }
+const inputNameStyle = { width : '120px' }
+const inputPriceStyle = { width : '50px' }
+
+
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = { 
-      editing: false, 
-      comments: [ "Lettuce" ]
+      groceries: [
+        {editing: true, name: "potatoe", price: "1", "categories": []},
+        {editing: false, name: "apple", price: "1", "categories": ["Fruit", "Bio"]},
+      ]
     };
   }
-
+  
   add(text) {
-    console.log('add dsafjhgfd' + text);
-    var arr = this.state.comments;
-    arr.push(text);
-    this.setState({comments: arr});
+    var arr = this.state.groceries;
+    arr.push(
+      { editing: false, 
+        name: this.refs.newName.value, 
+        price: this.refs.newPrice.value, 
+        "categories": [this.refs.newCategory.value]}
+      );
+    this.setState({groceries: arr});
   }
 
-  removeComment(i) {
-    console.log('removing comments ' + i);
-    var arr = this.state.comments;
-    arr.splice(i, 1);
-    this.setState({comments: arr})
-  }
+  
+  editingFalse(index, event) {
+    this.setState({...this.state, groceries : this.state.groceries.map((itemEach,idx) => {
+      return {...itemEach, editing: false} 
+    })
+  })}
+  
+  editingTrue(index, event) {
+    this.setState({...this.state, groceries : this.state.groceries.map((itemEach,idx) => {
+      if (index === idx) {
+        return {...itemEach, editing: true} 
+      } else {
+        return itemEach
+      }
+    })
+  })}
+  
+  upDateName(index, event) {
+    this.setState({...this.state, groceries : this.state.groceries.map((itemEach,idx) => {
+      if (index === idx) {
+        return {...itemEach, name: event.currentTarget.value} 
+      } else {
+        return itemEach
+      }
+    })
+  })}
+  
+  upDatePrice(index, event) {
+    this.setState({...this.state, groceries : this.state.groceries.map((itemEach,idx) => {
+      if (index === idx) {
+        return {...itemEach, price: event.currentTarget.value} 
+      } else {
+        return itemEach
+      }
+    })
+  })}
 
-  updateComment(newText, i) {
-    console.log('updating comment');
-    var arr = this.state.comments;
-    arr[i] = newText;
-    this.setState({comments: arr});
+  upDateCategory(index, event) {
+    this.setState({...this.state, groceries : this.state.groceries.map((itemEach,idx) => {
+      console.log(itemEach, event.currentTarget.value);
+      console.log(index);
+      if (index === idx) {
+        return {...itemEach, categories: event.currentTarget.value.split(",")} 
+      } else {
+        return itemEach
+      }
+    })
+  })}
+  
+  removeItem( index, event) {
+    this.setState({...this.state, groceries : this.state.groceries.filter((itemEach,idx) => {
+      return index !== idx
+    })})
   }
-
-  render() {
+    
+    render() {
+    
     return (
 				<div className="p-5">
           <h1>Grocery List</h1>
-          <button onClick={this.add.bind(null,'New Comment')} className="button-info create mb-3 btn-success">Add New</button>
-          <Board/>
+          <div className="d-inline-flex justify-content-center mb-2 p-3">
+            <div className="d-inline-flex">
+              <span className="mr-1">Name:</span><textarea ref="newName" className="mr-2" style={textAreaNameStyle}></textarea>
+              <span className="mr-1">Price:</span><textarea ref="newPrice" className="mr-2" style={textAreaPriceStyle}></textarea>
+              <span className="mr-1">Category:</span><textarea ref="newCategory" className="mr-2" style={textAreaNameStyle}></textarea>
+            </div>
+            <p><button onClick={this.add.bind(this)} className="button-info btn-success">Add New</button></p>
+          </div>
+          <div className="border px-1 py-2">
+            <h3>The Board</h3>
+              { this.state.groceries.map((eachItem, index) => {
+                return (
+                  <div key={index}>
+                    <div className="border d-flex justify-content-between p-1">
+                      <div className="pl-2">
+                        <strong><span>{eachItem.name}</span></strong>
+                        <span className="pl-2">$ {eachItem.price}</span>
+                        <span className="pl-2">
+                          {eachItem.categories.map((eachCategory, indexCategory) => 
+                            (<div key={indexCategory}>{eachCategory}</div>)
+                          )}
+                        </span>
+                      </div>
+                      <div className="pr-2">
+                          { 
+                            eachItem.editing
+                            ?
+                            <div className="">
+                                <label className="mx-1">Name:</label>
+                                <input value={eachItem.name} onChange={this.upDateName.bind(this,index)} style={inputNameStyle}/>
+                                <label className="mx-1">Price:</label>
+                                <input value={eachItem.price} onChange={this.upDatePrice.bind(this,index)} style={inputPriceStyle}/>
+                                <label className="mx-1">Category:</label>
+                                <input value={eachItem.categories.join(", ")} onChange={this.upDateCategory.bind(this,index)} style={inputNameStyle}/>
+                                <button onClick={this.editingFalse.bind(this,index)} className="button-success"  className="ml-2 btn-success">Save</button>
+                            </div>
+                            :
+                            <div className="">
+                              <button onClick={this.editingTrue.bind(this,index)} className="btn-primary mr-2">Edit</button>
+                              <button onClick={this.removeItem.bind(this,index)}className="btn-danger">Remove</button>
+                            </div>
+                          }
+                      </div>
+                    </div>
+                  </div>
+                )
+              })}
+          </div>
+          {JSON.stringify(this.state)}
         </div>
     );
   }
 }
 
-
-
-
-class Comment extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { editing: false }
-  }
-  edit() {
-    this.setState({editing: true});
-  }
-  save() {
-    console.log('got to saving comments');
-    this.props.updateCommentText(this.refs.newText.value, this.props.index);
-    this.setState({editing: false});
-  }
-  remove() {
-    console.log("removing comment");
-    this.props.deleteFromBoard(this.props.index)
-  }
-
-  render() {
-      {
-        if (this.state.editing) {
-          // return this.renderForm();
-          return (<RenderForm/>);
-        } else {
-          // return this.renderNormal();
-          return (<RenderNormal/>);
-        }
-      }
-  }
-}
-
-
-
-class RenderForm extends Component {
-    render() {
-      return (
-        <div>
-          <textarea ref="newText" defaultValue={this.props.children}></textarea>
-          <button onClick={this.save} className="button-success">Save</button>
-        </div>
-    );
-  }
-}
-
-class RenderNormal extends Component {
-  render() {
-    return (
-					<div className="commentContainer">
-						<div className="commentText">{this.props.children}</div>
-						<button onClick={this.edit} className="btn-primary mr-2">Edit</button>
-						<button onClick={this.remove} className="btn-danger">Remove</button>
-					</div>
-    );
-  }
-}
-
-class Board extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { 
-      editing: false, 
-      comments: [ "Apple" ]
-    };
-  }
-
-    add(text) {
-      console.log('add dsafjhgfd' + text);
-      var arr = this.state.comments;
-      arr.push(text);
-      this.setState({comments: arr});
-    }
-
-    removeComment(i) {
-      console.log('removing comments ' + i);
-      var arr = this.state.comments;
-      arr.splice(i, 1);
-      this.setState({comments: arr})
-    }
-
-		updateComment(newText, i) {
-      console.log('updating comment');
-      var arr = this.state.comments;
-      arr[i] = newText;
-      this.setState({comments: arr});
-		}
-
-    eachComment(text, i) {
-      return (
-        <Comment key={i} index={i} updateCommentText={this.updateComment} deleteFromBoard={this.removeComment}>
-          {text}
-        </Comment>
-        );	
-    }
-    
-  render() {
-      return (
-        <div className="border px-5 py-2">
-          <h3>The Board</h3>
-          { this.state.comments.map((eachComment, index) => {
-            return (
-              <div className="d-flex">
-                <div className="border d-block p-1">
-                  <h4>{eachComment}</h4>
-                  <Comment  key={index} updateCommentText={this.updateComment} deleteFromBoard={this.removeComment}/>
-                </div>
-              </div>
-            )
-          })}
-        </div>
-      );
-    }
-  }
 
 
   export default Home;
